4a1938fba2eba6002f0a779a44a963d62eccfcbb
Mahrosh-Farooq <rajagamerz2021@gmail.com>
2024-08-22 19:14:32 +0500

    done

--- projects/quiz game/script.js
+++ projects/quiz game/script.js
@@ -1,3 +1,12 @@
 const questions = [
 
+    {
+      question:"What does HTML stand for?",
+      answers: [
+        {text:"Hyper Text Multi Language", correct: false},
+        {text:"Hyper Text Multiple Language", correct: false},
+        {text:"Hyper Text Markup Language", correct: true },
+        {text:"Home Text Multi Language", correct: false},
+      ]
+    },
 ]
